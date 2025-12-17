2a4ece65e468e1cc5fed9d3e630d6dd4bc639b22
Nick Wellman <wellman.nicholas@gmail.com>
2025-12-17 09:14:22 -0500

    typos

--- src/db/202512161005__CreateUser.sql
+++ src/db/202512161005__CreateUser.sql
@@ -1,2 +1,2 @@
--- CREATE DATABASE IF NOT EXISTS food_library_db;
-create user 'grocery_user'@'%' identified by 'changeme';
+create
+user 'grocery_user'@'%' identified by 'changeme';
--- src/db/202512161009__CreateViews.sql
+++ src/db/202512161009__CreateViews.sql
@@ -1,5 +1,11 @@
-create or grocery_list view grocery_list_vw as
+create
+or replace view grocery_list_vw as
 select *
 from grocery_list;
-grant SELECT, INSERT , UPDATE, DELETE
-      on food_library_db.grocery_list_vw to 'grocery_user'@'%';
+grant
+SELECT,
+INSERT
+,
+UPDATE,
+DELETE
+on food_library_db.grocery_list_vw to 'grocery_user'@'%';
